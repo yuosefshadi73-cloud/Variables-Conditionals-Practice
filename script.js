4f205cd6a774ec34993aecd9a4438a1b38cc321f
yuosefshadi73-cloud <yuosefshadi73@gmail.com>
2026-09-24 10:13:02 +0300

    Add number classification function

---
+++ script.js
@@ -0,0 +1,16 @@
+function number(num){
+    if(num === 0){
+        console.log(`the number ${num} is zero`)
+    }else if(num > 0){
+        console.log(`the number ${num} is positive`)
+    }else{
+        console.log(`the number ${num} is negative`)
+    }
+}
+
+number(10);
+number(-5);
+number(0);
+
+
+
